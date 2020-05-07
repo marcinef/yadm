806df22bd82d4a2589a709c85b442ada8eae4274
Marcin Ef <mfigura@mintel.com>
2020-05-07 17:01:37 -0500

    update

--- install-packages.sh
+++ install-packages.sh
@@ -4,21 +4,20 @@ echo "Installing packages"
 
 sudo pacman -Sy --noconfirm \
   bluez \
+  git-crypt \
   bluez-libs \
+  pavucontrol \
   bluez-utils \
   vim \
   virt-manager \
   terminator \
   remmina \
-  nagstamon \
-  chrome \
   fbreader \
   qemu \
   nodejs \
   bash-completion \
   net-tools \
   bind-tools \
-  zoom \
   jq \
   perl-net-ldap-server \
   unzip \
@@ -47,7 +46,7 @@ sudo pacman -Sy --noconfirm \
   qemu-headless \
   rofi \
   sysstat \
-  the_silver_searcher \
+  the_silver_searcher
   ttf-dejavu
   ttf-font-icons \
   ttf-inconsolata \
@@ -55,39 +54,6 @@ sudo pacman -Sy --noconfirm \
   telepathy-glib \
   gnome-keyring\
   libvncserver \
-  ocsinventory-agent \
   yay 
 	
 
-yay -Sy --noconfirm \
-	acroread \
-	docker \
-	docker-machine-driver-kvm2 \
-	git-crypt \
-	google-cloud-sdk \
-	flameshot \
-	#kubectl-bin \
-    #    minikube \
-	pass \
-#	pavucontrol \
-	pcsclite \
-	powerline-fonts-git \
-	pwgen \
-	pulseaudio-module-bluetooth \
-	python-pipsi \
-	siege \
-	ranger \
-	ssss \
-	stress \
-	terraform \
-	terragrunt-bin \
-	tflint \
-	ttf-fira-code ttf-symbola \
-	stow \
-	urxvt-resize-font-git \
-	visual-studio-code-bin \
-	signal-desktop-bin \
-	xclip
-
-
-#pipsi install ansible
---
+++ install-yay-packages.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+echo "Installing YAY packages"
+
+yay -Sy --noconfirm \
+	acroread \
+    docker \
+	docker-machine-driver-kvm2 \
+    google-chrome \
+	git-crypt \
+	google-cloud-sdk \
+	flameshot \
+    kubectl-bin \
+	pavucontrol \
+	pcsclite \
+	pwgen \
+	python-pipsi \
+	siege \
+	ranger \
+	ssss \
+	stress \
+	terraform \
+	terragrunt-bin \
+	ttf-fira-code \
+	stow \
+	urxvt-resize-font-git \
+	visual-studio-code-bin \
+	signal-desktop-bin \
+    ocsinventory-agent \
+    dropbox \
+	xclip
+
+
+##pipsi install ansible
+#   zoom \
+#   nagstamon \
+	
+#	pass \
+	#tflint \
+    #ttf-symbola \
+    #    minikube \
